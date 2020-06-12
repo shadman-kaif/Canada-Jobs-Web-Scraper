def6da9e287a98b5c8afc732396521e862c61b4e
Shadman Kaif <kaifshadman@gmail.com>
2020-06-12 11:57:38 -0400

    Added extensive error checking for Location

--- spiders/recursion.py
+++ spiders/recursion.py
@@ -31,6 +31,7 @@ class RecursionSpider(scrapy.Spider):
         jobID = response.css('span::text').extract()
         noc_number = response.css("span.noc-no::text").extract()
 
+        # Salary extracts, min and max salaries are ranges for jobs
         salary_min_value = response.css("ul.job-posting-brief.colcount-lg-2 span[property*='minValue']::text").extract()
         salary_max_value = response.css("ul.job-posting-brief.colcount-lg-2 span[property*='maxValue']::text").extract()
         salary_unit = response.css("ul.job-posting-brief.colcount-lg-2 span[property*='unitText']::text").extract()
@@ -40,23 +41,21 @@ class RecursionSpider(scrapy.Spider):
         # Removing unnecessary whitespace and words from strings in list
         date = map(lambda s: s.strip(), date)
         date = map(lambda s: s.strip(" Posted on "), date)
-        postal_code = [x.strip(' ') for x in postal_code]
 
         # Removing the comma at the end of the location as it's extracted with a comma at the end
         location = [x[:-1] for x in location]
 
         # Going through span tag to find the vacancy number
         vacancy = [str for str in vacancy if "Vacanc" in str]
-        vacancy.remove('Vacancies') # Remove one instance of "Vacancies" string that comes before
+        vacancy.remove('Vacancies') # Remove one instance of "Vacancies" string that comes in extract before
 
         # Status and duration cleaning
         del status[1]
         del duration[0]
-        duration = map(lambda s: s.strip(), duration)
+        duration = [''.join(item.split()) for item in duration]
 
         # Checking for 146 string in Job ID
         jobID = [str for str in jobID if "147" in str]
-
         if len(jobID) == 0:
             jobID = response.css('span::text').extract()
             jobID = [str for str in jobID if "146" in str]
@@ -73,6 +72,9 @@ class RecursionSpider(scrapy.Spider):
             jobID = response.css('span::text').extract()
             jobID = [str for str in jobID if "142" in str]
         if len(jobID) == 0:
+            jobID = response.css('span::text').extract()
+            jobID = [str for str in jobID if "148" in str]
+        if len(jobID) == 0:
             jobID = [""]
 
         if len(business) == 0:
@@ -81,11 +83,19 @@ class RecursionSpider(scrapy.Spider):
         if len(hours) == 0:
             hours = [""]
 
+        # Stripping of "hours per week" string which is not needed
         hours = map(lambda s: s.strip("hours per week"), hours)
 
+        # Adding dollar assign before the salaries
         sal_max = ["$" + salary_max_value for salary_max_value in salary_max_value]
         sal_min = ["$" + salary_min_value for salary_min_value in salary_min_value]
 
+        # Stripping of the "NOC" keyword from NOC Number extract
+        noc_number = map(lambda s: s.strip("NOC "), noc_number)
+
+        """
+        Error Checking
+        """
         if len(sal_max) == 0:
             sal_max = [""]
         
@@ -97,9 +107,16 @@ class RecursionSpider(scrapy.Spider):
         
         salary_unit = [x.lower() for x in salary_unit]
 
-        noc_number = map(lambda s: s.strip("NOC "), noc_number)
+        if len(location) == 0:
+            response.css("p.nomargin::text").extract()
+            location = [''.join(item.split()) for item in location]
 
         if len(location) == 0:
+            location = response.css("span.city::text").extract()
+            location = [''.join(item.split()) for item in location]
+            location = [str for str in location if ",ON" in str]
+
+        if len(location) == 0: 
             location = [""]
 
         if len(vacancy) == 0: 
@@ -107,7 +124,6 @@ class RecursionSpider(scrapy.Spider):
 
         if len(postal_code) == 0:
             postal_code = [""]
-
 
         # Output 
         for item in zip(title, date, location, business, vacancy, status, duration, jobID, hours, sal_min, sal_max, salary_unit, noc_number):
